5d7dafb9b946b94c9cbce5e9378f488391edbf88
prakashtsx <urprakashgupta@gmail.com>
2026-01-17 13:34:22 +0530

    implemented login route

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { pool } from "../db/index.js";
 export const signup = async (req, res) => {
   const { username, password } = req.body;
@@ -44,6 +45,60 @@ export const signup = async (req, res) => {
     return res.status(500).json({
       success: false,
       error: "something went wrong",
+    });
+  }
+};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  //validate inputs
+  if (!username || !password) {
+    return res.status(400).json({
+      success: false,
+      error: "invalid inputs",
+    });
+  }
+
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        success: false,
+        error: "user does not exist",
+      });
+    }
+    const user = result.rows[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        error: "incorrect password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+    );
+    return res.status(200).json({
+      success: true,
+      data: {
+        message: "Login successful",
+        token,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: "server error",
     });
   }
 };
